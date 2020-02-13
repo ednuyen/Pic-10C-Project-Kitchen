370419ad47be9a6b0b727e6c05b45d286912f580
ednuyen <60238185+ednuyen@users.noreply.github.com>
2020-02-13 13:29:14 -0800

    update MainWindow.cpp
    has instructions button, changed function names to be more explicit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,53 +5,63 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)   
 {
-
-//    QPixmap pm(":/menu/pic10c-menu-#3-pixilart.png");
-//    this->title->setPixmap(pm);
-//    this->title->setScaledContents(true);
-
     QPixmap bkgnd(":/menu/pic10c-menu-#3-pixilart.png");
     bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
     QPalette palette;
     palette.setBrush(QPalette::Background, bkgnd);
     this->setPalette(palette);
-
     layout = new QVBoxLayout();
     mainscreen = new QVBoxLayout();
     buttons = new QHBoxLayout();
 
-  //title = new QLabel("HECK'S KITCHEN");
-  //challenge_level = new QLabel("Pick the Spice level you want to play!!");
-  challenge = new QSlider();
-  challenge->setRange(1,3);
-  start = new QPushButton("Start the game!!");
-  leave = new QPushButton(" Quit!");
-  connect(start, &QPushButton::pressed, this,&MainWindow::buttonpressed);
-   connect(leave,&QPushButton::pressed, this, &MainWindow::buttonclose);
+    challenge = new QSlider();
+    challenge->setRange(1,3); // choose difficulty of game
+    start = new QPushButton("PLAY");
+    start->setStyleSheet("QPushButton { color : black; }");
+    leave = new QPushButton("EXIT");
+    leave->setStyleSheet("QPushButton { color : black; }");
+    QPushButton* help = new QPushButton("INSTRUCTIONS");
+    help->setStyleSheet("QPushButton { color : black; }");
+    connect(start,&QPushButton::pressed, this, &MainWindow::pressedPlay);
+    connect(leave,&QPushButton::pressed, this, &MainWindow::pressedExit);
+    connect(help,&QPushButton::pressed, this, &MainWindow::pressedHelp);
 
-  //layout->addWidget(title);
- // layout->addWidget(challenge_level);
-  layout->addWidget(challenge);
+    layout->addWidget(challenge);
+    buttons->addWidget(leave);
+    buttons->addWidget(help);
+    buttons->addWidget(start);
+    mainscreen->addLayout(layout);
+    mainscreen->addLayout(buttons);
 
-this->setFixedSize(700,700);
-  buttons->addWidget(start);
-  buttons->addWidget(leave);
-  mainscreen->addLayout(layout);
-  mainscreen->addLayout(buttons);
-  QWidget* central = new QWidget();
-      central->setLayout(mainscreen);
-      this->setCentralWidget(central);
+    QWidget* central = new QWidget();
+    central->setLayout(mainscreen);
+    this->setCentralWidget(central);
 }
 
-MainWindow::~MainWindow(){}
+MainWindow::~MainWindow() {}
 
-void MainWindow::buttonpressed(){
-
-  w2.show();
+void MainWindow::pressedPlay(){
+    w2.show();
 }
 
-void MainWindow::buttonclose(){
-MainWindow::close();
-w2.close();
+void MainWindow::pressedExit(){
+    MainWindow::close();
+    w2.close();
 }
+
+void MainWindow::pressedHelp() {
+//    stackedWidget->setCurrentIndex(1);
+    i.show();
+}
+
+QHBoxLayout *MainWindow::getButtons() const
+{
+    return buttons;
+}
+
+void MainWindow::setButtons(QHBoxLayout *value)
+{
+    buttons = value;
+}
+
 
